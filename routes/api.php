188e415e64e4ca29f228ecada092dd35a105b68a
Ricardo <ricardojoseajedrez@gmail.com>
2024-06-13 08:14:32 -0400

    add patch method

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
 Route::put('/tasks/{task}', [TaskController::class,'update']);
+Route::patch('/tasks/{task}/complete', [TaskController::class,'complete']);
 Route::delete('/tasks/{task}', [TaskController::class,'destroy']);
