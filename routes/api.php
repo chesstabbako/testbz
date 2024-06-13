2eb83ad94020444d4880b9fd1f38e78dd31f3bf2
Ricardo <ricardojoseajedrez@gmail.com>
2024-06-13 07:57:28 -0400

    Add put and delete method

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('//tasks', [TaskController::class, 'index']);
+Route::get('/tasks', [TaskController::class, 'index']);
 Route::post('/tasks', [TaskController::class, 'store']);
+Route::put('/tasks/{task}', [TaskController::class,'update']);
+Route::delete('/tasks/{task}', [TaskController::class,'destroy']);
